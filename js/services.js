39b3106577498338802d71e4bb6323cb9fa70942
Gyuwon Yi <gyuwon@live.com>
2013-12-14 03:55:24 -0500

    12/14/2013

--- js/services.js
+++ js/services.js
@@ -74,7 +74,7 @@ var services = {
                 contents.push(content);
                 content.id = contents.length;
                 callback();
-            })
+            });
         };
     },
     
